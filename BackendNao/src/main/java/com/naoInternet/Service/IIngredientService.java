b0be2f9222bdd001a21b1eb48dc75929c11fcb6a
Josue Huerta <gjhuerta_20@hotmail.com>
2021-12-27 12:04:52 -0500

    Third Update
    All unnecessary files was deleted

--- BackendNao/src/main/java/com/naoInternet/Service/IIngredientService.java
+++
@@ -1,9 +0,0 @@
-package com.naoInternet.Service;
-
-import com.naoInternet.Entity.Ingredient;
-
-import java.util.List;
-
-public interface IIngredientService extends CrudService<Ingredient>{
-    public List<Ingredient> findByName(String name)throws Exception;
-}
--- BackendNao/src/main/java/com/naoInternet/Service/IRecipeIngredientService.java
+++
@@ -1,9 +0,0 @@
-package com.naoInternet.Service;
-
-import com.naoInternet.Entity.RecipeIngredient;
-
-import java.util.List;
-
-public interface IRecipeIngredientService extends CrudService<RecipeIngredient>{
-    public List<RecipeIngredient> findByQuantity(Integer quantity)throws Exception;
-}
